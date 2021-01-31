d1cbbe3e8be47792fc853479e869db93448639da
dantb <dantb@hotmail.co.uk>
2021-01-31 11:48:09 +0000

    Delete calculator - use spreadsheet instead

--- student-debt-calculator/src/main/scala/calculator/Constants.scala
+++
@@ -1,8 +0,0 @@
-package calculator
-
-import model.Percentage
-
-object Constants:
-  val PercentageOfEarningsOverThreshold = Percentage(9)
-  val MonthsInYear = 12
-  val MaxSalaryBasedInterest = 3
